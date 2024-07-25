5b1fa33a71d3bff9fa9999834a51e4639cae6407
Kartik Gupta <119871426+anythingkartik@users.noreply.github.com>
2024-07-25 12:38:48 +0530

    Time: 1 ms (81.04%), Space: 45.3 MB (75.7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -32,3 +32,22 @@ class Solution {
     }
 
 }
+/*
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix.length;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    for(int k=0;k<matrix.length;k++)
+                    {
+                        matrix[i][k]=0;
+                        matrix[k][j]=0;
+                    }
+                }
+            }
+        }
+    }
+}*/
