4e1ccae69df9ef847113494213b5afa8830ec705
Kartik Gupta <119871426+anythingkartik@users.noreply.github.com>
2024-10-03 22:54:25 +0530

    Time: 1 ms (99.79%), Space: 46.1 MB (48.34%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result=0;
-        for(int i=0;i<nums.length;i++) {  //for (int num : nums) Directly access elements
+        for(int i=0;i<nums.length;i++) {  //for (int num : nums) Directly access all
             result=result^nums[i];
         }
         return result;
