25518bb65f6405806c9e1258be134b491ffc8072
Kartik Gupta <119871426+anythingkartik@users.noreply.github.com>
2025-01-01 23:59:18 +0530

    Time: 2 ms (11.98%), Space: 42 MB (24.21%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,18 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s=s.trim();
-        int pos=s.lastIndexOf(' ');
-        int len=(s.substring(pos+1)).length();
-        return len;
+        // Remove extra spaces and trim the string
+        String str = s.replaceAll("\\s+", " ").trim();
+        
+        // Find the index of the last space
+        int ind = str.lastIndexOf(' ');
+        
+        // If there is no space, the whole string is the last word
+        if (ind == -1) {
+            return str.length();
+        }
+        
+        // Get the substring after the last space and return its length
+        String wrd = str.substring(ind + 1);
+        return wrd.length();
     }
 }
