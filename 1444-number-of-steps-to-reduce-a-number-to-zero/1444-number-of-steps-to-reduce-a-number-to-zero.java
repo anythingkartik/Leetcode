d82d9d5145f01f43c4ae9c84f21a3d73f5bd6639
Kartik Gupta <119871426+anythingkartik@users.noreply.github.com>
2025-01-11 18:25:53 +0530

    Time: 0 ms (100%), Space: 40.9 MB (9.41%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,6 +1,20 @@
 class Solution {
     public int numberOfSteps(int n) {
+        int ans=counting(n,0);
         int count=0;
+        return ans;
+    }
+        public int counting(int n,int count) {
+        if(n==0) {
+            return count;
+        }
+        else if(n%2==0) {
+            return counting(n/2,count+1);
+        }
+        return counting(n-1,count+1);
+        }
+
+       /* int count=0;
         while(n!=0) {
             if(n%2==0){
                 count++;
@@ -11,6 +25,6 @@ class Solution {
                 count++;
             }
         }
-        return count;
-    }
+        return count;*/
+    
 }
