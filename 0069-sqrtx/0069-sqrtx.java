d03baee0e8ef6bc413b2707745a8bcbc580cbc9c
Kartik Gupta <119871426+anythingkartik@users.noreply.github.com>
2025-01-26 00:55:43 +0530

    Time: 1 ms (87.12%), Space: 40.9 MB (67.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,27 @@
 class Solution {
     public int mySqrt(int x) {
-        int n=(int)Math.sqrt(x);
-        return n;
+        int s=0;
+        int e=x;
+        int ans=0;
+        int mid=0;
+
+        if(x==0 || x==1) {
+            return x;
+        }
+        else{
+
+            while(s<=e) {
+            mid=s+(e-s)/2;
+
+            if(mid <= x / mid) {
+                ans=mid;
+                s=mid+1;
+            }
+            else {
+                e=mid-1;
+            }
+            }
+        }
+    return ans;
     }
 }
