132107d96d372a0cfab6f647244cad63c05ab9f2
Kartik Gupta <119871426+anythingkartik@users.noreply.github.com>
2025-04-11 02:44:17 +0530

    Time: 5 ms (25.84%), Space: 45.3 MB (71.16%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int s = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(s != nums[i]) {
+                return s;
+            }
+            s++;
+        }
+        return s;
+    }
+}
